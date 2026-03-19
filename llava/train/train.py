6af92f872060a96816a747e071ba2f245930f76f
Bocheng Zou <bocheng.zou@outlook.com>
2026-03-19 16:59:54 -0500

    Remove unnecessary formatting change

--- llava/train/train.py
+++ llava/train/train.py
@@ -964,7 +964,7 @@ def train(attn_implementation=None):
         
         vision_tower = model.get_vision_tower()
         vision_tower.to(dtype=torch.bfloat16 if training_args.bf16 else torch.float16, device=training_args.device)
-        
+
         data_args.image_processor = vision_tower.image_processor
         data_args.is_multimodal = True
 
